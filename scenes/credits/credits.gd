5288d2687ba000268a5f4ece1a68d5c477ab44a4
Altro31 <albe020531@outlook.com>
2025-03-09 15:08:35 -0400

    fix: credits

--- scenes/credits/credits.gd
+++ scenes/credits/credits.gd
@@ -1,4 +1,7 @@
 extends Node2D
 
+func _ready() -> void:
+	get_tree().paused = false
+
 func _process(delta: float) -> void:
 	$Control.position.y -= 1
--- scenes/game_scene/credits.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _physics_process(delta: float) -> void:
-	print("Hola")
-	$Control.position.y -= 1
