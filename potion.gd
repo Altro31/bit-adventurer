6c6afcd24e10d535db454bfcc1bb2a263fc1c933
Alberto Hdez <albertojhdez01@gmail.com>
2025-03-19 10:53:57 -0400

    fix

--- potion.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-func _on_player_entered(body):
-	if body is Player:
-		$Sprite.play("off")
-		$Collision.set_deferred("disabled",true)
--- scenes/entities/potion.gd
+++ scenes/entities/potion.gd
@@ -1,1 +1,7 @@
 extends Area2D
+
+func _on_player_entered(body):
+	if body is Player:
+		body.speed = 250
+		$Sprite.play("off")
+		$Collision.set_deferred("disabled",true)
